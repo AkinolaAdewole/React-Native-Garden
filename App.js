a7755ae0dead753f5b9576b9fb224a42b4dd97d4
Akinola Adewole <adewoleakinola24@gmail.com>
2025-03-18 23:53:10 +0100

    emulation using Android studio

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Akinola is now a senior software developer.</Text>
+      <Text>Hello React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
