0b7d22d96a7e39d1f432a64ec95afbd181979001
Akinola Adewole <adewoleakinola24@gmail.com>
2025-03-18 23:55:11 +0100

    emulation using Android studio

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("App executed");
+  
   return (
     <View style={styles.container}>
       <Text>Hello React Native</Text>
