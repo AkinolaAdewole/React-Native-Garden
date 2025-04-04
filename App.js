8b66083c2823fd56bf7538981baf2042239a82a1
Akinola Adewole <adewoleakinola24@gmail.com>
2025-04-04 23:56:07 +0100

    expo components explained further

--- App.js
+++ App.js
@@ -47,7 +47,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
